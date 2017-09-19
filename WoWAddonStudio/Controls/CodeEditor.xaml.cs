a4236f7aecabd5a7bc7ff69baa1df3da89edf055
Frankenhooker <ichstinkedollwiekacke@googlemail.com>
2017-09-19 06:49:09 +0200

    first iteration of the  autocompletion feature

--- WoWAddonStudio/Controls/CodeEditor.xaml.cs
+++ WoWAddonStudio/Controls/CodeEditor.xaml.cs
@@ -1,8 +1,15 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Xml;
+using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Editing;
 using ICSharpCode.AvalonEdit.Highlighting;
 using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using WoWAddonStudio.Definitions;
 
 namespace WoWAddonStudio.Controls
 {
@@ -11,10 +18,16 @@ namespace WoWAddonStudio.Controls
     /// </summary>
     public partial class CodeEditor : UserControl
     {
+        CompletionWindow completionWindow;
+        LuaCompletionData LCD = new LuaCompletionData("");
+
         public CodeEditor()
         {
             InitializeComponent();
             InitializeHighlighter();
+            Editor.TextArea.TextEntering += textEditor_TextArea_TextEntering;
+            Editor.TextArea.TextEntered += textEditor_TextArea_TextEntered;
+            LCD.InitializeData();
         }
 
         /// <summary>
@@ -30,6 +43,50 @@ namespace WoWAddonStudio.Controls
                 reader.Close();
                 stream.Close();
             }
+        }
+
+        void textEditor_TextArea_TextEntered(object sender, TextCompositionEventArgs e)
+        {
+            foreach (string str in LCD.InputListLua)
+            {
+                if (e.Text == str)
+                {
+                    ManageCompletion(e.Text);
+                }
+            }
+        }
+
+        void textEditor_TextArea_TextEntering(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text.Length > 0 && completionWindow != null)
+            {
+                if (!char.IsLetterOrDigit(e.Text[0]))
+                {
+                    // Whenever a non-letter is typed while the completion window is open,
+                    // insert the currently selected element.
+                    completionWindow.CompletionList.RequestInsertion(e);
+                }
+            }
+            // Do not set e.Handled=true.
+            // We still want to insert the character that was typed.
+        }
+
+        void ManageCompletion(string TextInput)
+        {
+            // Open code completion after the user has gives TextInput:
+            completionWindow = new CompletionWindow(Editor.TextArea);
+            IList<ICompletionData> data = completionWindow.CompletionList.CompletionData;
+            LCD.GetCompletionTextList(TextInput);
+            completionWindow.StartOffset -= 1;  // Don't kick out typed code
+            foreach (string str in LCD.CompletionTextList)
+            {
+                data.Add(new LuaCompletionData(str));
+            }
+            completionWindow.Show();
+            completionWindow.Closed += delegate
+            {
+                completionWindow = null;
+            };
         }
     }
 }
---
+++ WoWAddonStudio/Definitions/LuaCompletionData.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Editing;
+using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace WoWAddonStudio.Definitions
+{
+    public class LuaCompletionData : ICompletionData
+    {
+        public List<string> InputListLua = new List<string>();
+        public List<string> CompletionTextList = new List<string>();
+
+        public LuaCompletionData(string text)
+        {
+            this.Text = text;
+        }
+
+        public void InitializeData()
+        {
+            InputListLua.Add(".");
+            InputListLua.Add(":");
+            InputListLua.Add("f");
+            InputListLua.Add("t");
+            InputListLua.Add("l");
+        }
+
+        public void GetCompletionTextList(string Input)
+        {
+            CompletionTextList.Clear();
+            switch (Input)
+            {
+                case ".":                                   // Operators
+                    CompletionTextList.Add(".Text1");
+                    CompletionTextList.Add(".Text2");
+                    break;
+                case ":":
+                    CompletionTextList.Add(".Text1");
+                    CompletionTextList.Add(".Text2");
+                    break;
+                case "f":                                 // Keywords
+                    CompletionTextList.Add("for");
+                    CompletionTextList.Add("function");
+                    break;
+                case "t":
+                    CompletionTextList.Add("true");
+                    break;
+                case "l":
+                    CompletionTextList.Add("local");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public System.Windows.Media.ImageSource Image
+        {
+            get { return null; }
+        }
+
+        public string Text { get; private set; }
+
+        public object Content
+        {
+            get { return this.Text; }
+        }
+
+        public object Description
+        {
+            get { return "Description for " + this.Text; }
+        }
+
+        public double Priority { get { return 0; } }
+
+        public void Complete(TextArea textArea, ISegment completionSegment, EventArgs insertionRequestEventArgs)
+        {
+            textArea.Document.Replace(completionSegment, this.Text);
+        }
+    }
+}
